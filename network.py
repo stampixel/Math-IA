e61ee872ea626d9d46c249f15b080a1030c232db
bourrasque <stampixel.t@gmail.com>
2023-12-10 15:46:49 -0500

    Updated network.py

--- network.py
+++ network.py
@@ -9,3 +9,43 @@ class Network:
         self.layers.append(layer)
 
     # set loss to use
+    def use(self, loss, loss_prime):
+        self.loss = loss
+        self.loss_prime = loss_prime
+
+    # predict output for given input
+    def predict(self, input_data):
+        # sample dimension first
+        samples = len(input_data)
+        result = []
+        # run network over all samples
+        for i in range(samples):
+            # forward propagation
+            output = input_data[i]
+            for layer in self.layers:
+                output = layer.forward_propagation(output)
+            result.append(output)
+        return result
+
+    # trianing
+    def fit(self, x_train, y_train, epochs, learning_rate):
+        samples = len(x_train)
+
+        # training loop
+        for i in range(epochs):
+            err = 0
+            for j in range(samples):
+                # forward propagation
+                output = x_train[j]
+                for layer in self.layers:
+                    output = layer.forward_propagation(output)
+                # compute loss
+                err += self.loss(y_train[j], output)
+
+                # backward propagation
+                error = self.loss_prime(y_train[j], output)
+                for layer in reversed(self.layers):
+                    err = layer.backward_propagation(error, learning_rate)
+            # calculate averasge error on all samples
+            error /= samples
+            print('epoch %d/%d   error=%f' % (i + 1, epochs, err))
